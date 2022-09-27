14b6c8d652913b34ec3abb383952810650c099a4
yfurkanp <furkan_polat99@hotmail.com>
2022-09-27 18:31:54 +0300

    debtSystemV1

---
+++ src/main/java/com/debtSystem/debtSystem/DebtSystemApplication.java
@@ -0,0 +1,13 @@
+package com.debtSystem.debtSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DebtSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DebtSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/debtSystem/debtSystem/DebtSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.debtSystem.debtSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DebtSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
