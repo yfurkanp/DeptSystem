1f69cf355ce655211360645fb8aabf076a7741f1
yfurkanp <furkan_polat99@hotmail.com>
2022-09-27 19:24:37 +0300

    added model debt

---
+++ src/main/java/com/debtSystem/debtSystem/model/Debtor.java
@@ -0,0 +1,26 @@
+package com.debtSystem.debtSystem.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name="table_debtor")
+
+@Data
+@NoArgsConstructor
+public class Debtor {
+		@Id
+		@GeneratedValue(strategy = GenerationType.IDENTITY)
+		private int Id;
+		private String Name;
+		private String Phone;
+		private String Addres;
+		private int debt;
+		
+}
